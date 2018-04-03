6dade45e86bc0c81a7f11327a592208f736ad2cd
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2018-04-03 19:30:36 -0400

    Payroll Console Application v.01
    initial commit

---
+++ payrollApp/payrollApp v01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollApp1
+{
+    class Program
+    {
+        static string name;
+        double wage;
+        double hours;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void input(string[] args)
+        {
+            Console.WriteLine("What is employees name?");
+            name = Console.ReadLine();
+
+        }
+        static void arrays(string[] args)
+        {
+            string[] names;
+        }
+    }
+}
