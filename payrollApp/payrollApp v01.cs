d7dc403d07919f8898b72e8479c91e1af33cbe7e
mrparmer <37225891+mrparmer@users.noreply.github.com>
2018-04-04 15:21:16 -0400

    add input to list
    added code to add user input to appropriate list.

--- payrollApp/payrollApp v01.cs
+++ payrollApp/payrollApp v01.cs
@@ -17,21 +17,26 @@ namespace PayrollApp1
             
         }
 
+        static void lists(string[] args)
+        {
+            List<string> nameList = new List<string>();
+            List<double> hoursList = new List<double>();
+            List<double> WageList = new List<double>();
+        }
+
         static void input(string[] args)
         {
             Console.WriteLine("What is employees name?");
             name = Console.ReadLine();
+            nameList.Add(name);
             Console.WriteLine("What was " + name + " hours worked?");
             hours = Convert.ToDouble(Console.ReadLine());
+            hoursList.Add(hours);
             Console.WriteLine("Pay rate for " + name + "?");
             wage = Convert.ToDouble(Console.ReadLine());
-           
+            wagesList.Add(wage);
         }
-        static void lists(string[] args)
-        {
-            List<string> names = new List<string>();
-            List<int> hoursWorked = new List<int>();
-            List<int> payRate = new List<int>();
-        }
+
+        
     }
 }
