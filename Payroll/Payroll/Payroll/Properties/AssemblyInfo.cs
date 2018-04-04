aa7051bd8ddda79970e651087a319d84ec180641
mrparmer <37225891+mrparmer@users.noreply.github.com>
2018-04-04 15:43:32 -0400

    Correcting file

--- Payroll/Payroll/Payroll/Properties/AssemblyInfo.cs
+++ Payroll/Payroll/Payroll/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Payroll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Payroll")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4d911273-c08a-45cb-bca2-f82d4a17cec2")]
+[assembly: Guid("f5d57d58-cbc6-43ae-857a-555c2bd89507")]
 
 // Version information for an assembly consists of the following four values:
 //
