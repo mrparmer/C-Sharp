400de221efb3c1a06205926469e0d171772c6612
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2018-09-25 09:17:39 -0400

    CS for Beginners Commit

---
+++ CS for Beginners Book/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.Write("Welcome " + name);
+            Console.Write("\nWhat is your age? ");
+            var age = Console.ReadLine();
+            Console.WriteLine("\n\tName:\tAge:");
+            Console.WriteLine("\t{0} \t{1}", name, age);
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
