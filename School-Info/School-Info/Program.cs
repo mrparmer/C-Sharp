efd4da258be9e81a5552358297e91ad510fa707d
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2019-04-15 22:37:30 -0400

    Simple app
    Just a simple app I created since I didn't have much time based on a posted homework assignment I found. Did it just so I could scratch the code itch.

---
+++ School-Info/School-Info/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace School_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Asterix());
+            Console.WriteLine(SchoolInformation());
+            Console.WriteLine(Asterix());
+        }
+
+        static string Asterix()
+        {
+            return "**************************************************";
+        }
+
+        static string SchoolInformation ()
+        {
+            string schoolName = "Kennesaw State University";
+            string schoolCountry = "USA";
+            int schoolCost = 15000;
+            int numOfStudents = 2000;
+
+            string schoolInfo = String.Format("School Name:         {0}\n" + 
+                                              "School Country:      {1}\n" + 
+                                              "School Cost:         {2:C2}\n" + 
+                                              "Student Population:  {3}", 
+                                              schoolName, schoolCountry, schoolCost, numOfStudents);
+
+            return schoolInfo;
+        }
+    }
+}
