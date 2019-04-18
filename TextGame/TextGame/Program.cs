e46e57c34ba622b81892a3d0e61aea92e7788305
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2019-04-17 21:42:55 -0400

    First choice
    Just had a few minutes tonight to work, so created the first if statements, handling the opening of the story.

--- TextGame/TextGame/Program.cs
+++ TextGame/TextGame/Program.cs
@@ -17,8 +17,51 @@ namespace TextGame
 
             Console.WriteLine("Welcome to Aluriat " + playerName);
             Console.WriteLine(playerName + " has " + player.ToString() + " to start with. As you adventure you will get into battles, " +
-                "\ndon't forget to rest after each or your health won't regenerate." +
-                "\nYou will also find powerful items that will increase your health, watch out for those!");
+                             "\ndon't forget to rest after each or your health won't regenerate." +
+                             "\nYou will also find powerful items that will increase your health, watch out for those!\n");
+
+            Console.WriteLine("The story begins; \n" +
+                              "You wake up, alone, on a lonely beach, with no memory of how you got there. \n" +
+                              "What do you do?\n");
+
+            Console.WriteLine("(T)ake stock of your surroundings, and what you have on you. \n" +
+                              "(S)hout and see if anyone is around.\n" +
+                              "(B)egin walking, looking for signs of people.\n");
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput == "T")
+                {
+                    Console.WriteLine("You look around you and notice that there are no footprints leading to where you were laying, " +
+                                      "\nthere are no sticks, shells, there is nothing. \n" +
+                                      "You pat yourself down and discover you are wearing nothing but a thin overshirt, and light pants.\n" +
+                                      "It's starting to get cold.\n");
+
+                    break;
+                }
+                else if (userInput == "S")
+                {
+                    Console.WriteLine("You yell. You shout until you are hoarse, and no one is answering you, you are well and truly alone, it's starting to get cold");
+                    Console.WriteLine("What do you do?");
+
+                    break;
+                }
+                else if (userInput == "B")
+                {
+                    Console.WriteLine("You being to walk, it's plain to you that this won't be a short walk, the land before you is flat, with forest as far as you can see.\n" +
+                                      "As you walk you begin to realize it's pretty late in the day, and it's starting to get cold.");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid choice, please select a valid choice.");
+                    
+                }
+
+            }
         }
+
     }
 }
