478a62e655bcecadbebf9080b91b742fb2bf39a4
mrparmer <37225891+mrparmer@users.noreply.github.com>
2019-05-07 15:08:07 -0400

    Fleshed out choice 1-1-1
    I have got to find a better naming schema, and get over the motivational hump of writing the story. I love writing the code, but the story is giving me fits.

--- TextGame/TextGame/choice1-1.cs
+++ TextGame/TextGame/choice1-1.cs
@@ -7,11 +7,11 @@ namespace TextGame
     public class choice1_1
     {
         int playerHealth;
-        
-    PlayerClass player = new PlayerClass();
+
+        PlayerClass player = new PlayerClass();
         public void choice1()
 
-            //player choice, takes stock of surroundings.
+        //player choice, takes stock of surroundings.
 
         {
             Console.WriteLine("You look around you and notice that there are no footprints leading to where you were laying,\n" +
@@ -22,61 +22,62 @@ namespace TextGame
                               "1. Scream for help.\n" +
                               "2. Start walking.\n" +
                               "3. Stand still, calm your breathing, and listen.");
-        
-        playerHealth = player.health;
 
-        if (playerHealth < 100)
-        {
-            Console.WriteLine("4. Rest\n");
-        }
+            playerHealth = player.health;
 
-        choices();
+            if (playerHealth < 100)
+            {
+                Console.WriteLine("4. Rest\n");
+            }
+
+            choices();
         }
         public void choices()
         {
-                int userInput = Int32.Parse(Console.ReadLine());
-                if (userInput == 1)
-                {
-                    Console.WriteLine("You yell. You shout until you are hoarse, and no one is answering you, you are well and truly alone.\n" +
-                                      "There is no one here, all you managed to do was attract the attention of something in the woods, it's coming this way.\n" + 
-                                      "What do you do now?\n" +
-                                      "1. Run into the water, maybe it will lose you in the waves. The water is frigid.\n" +
-                                      "2. There... There... is a monster?!?\n" +
-                                      "3. Attempt to bury yourself in the sand.");
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("You yell. You shout until you are hoarse, and no one is answering you, you are well and truly alone.\n" +
+                                  "There is no one here, all you managed to do was attract the attention of something in the woods, it's coming this way.\n" +
+                                  "What do you do now?\n" +
+                                  "1. Run into the water, maybe it will lose you in the waves. The water is frigid.\n" +
+                                  "2. There... There... is a monster?!?\n" +
+                                  "3. Attempt to bury yourself in the sand.");
                 choices1_1();
-                
-                }
-                else if (userInput == 2)
-                {
-                    Console.WriteLine("You are exerting yourself, you have " + player.Exertion() + " health\n" +
-                                      "You being to walk, it's plain to you that this won't be a short walk, the land before you has a slight rise, \n" +
-                                      "you know that this will make the journey tiring, with forest as far as you can see to both sides of you.\n" +
-                                      "As you walk you begin to realize it's pretty late in the day, and it's starting to get cold.\n " +
-                                      "What do you do now?\n" +
-                                      "1. Continue walking, keeping a steady pace.\n" +
-                                      "2. Start running, it's getting cold, the exertion should keep you warmer.\n" +
-                                      "3. Succumb to the inevitable, there is no escape, sit down and wait for the cold.");
+
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("You are exerting yourself, you have " + player.Exertion() + " health\n" +
+                                  "You being to walk, it's plain to you that this won't be a short walk, the land before you has a slight rise, \n" +
+                                  "you know that this will make the journey tiring, with forest as far as you can see to both sides of you.\n" +
+                                  "As you walk you begin to realize it's pretty late in the day, and it's starting to get cold.\n " +
+                                  "What do you do now?\n" +
+                                  "1. Continue walking, keeping a steady pace.\n" +
+                                  "2. Start running, it's getting cold, the exertion should keep you warmer.\n" +
+                                  "3. Succumb to the inevitable, there is no escape, sit down and wait for the cold.");
                 choices1_2();
-                }
-                else if (userInput == 3)
-                {
-                    Console.WriteLine("As your pulse calms, your breathing slows, and you focus on the sounds around you, you think you hear the sound of heavy foot steps prowling closer.\n" +
-                                      "They are still a little ways away, for now.\n" +
-                                      "What do you do now?\n " +
-                                      "1. Panic, run, hope it doesn't catch you.\n" +
-                                      "2. Remain calm, look for a hiding place, anywhere to disappear.\n" +
-                                      "3. Stand and fight.");
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine("As your pulse calms, your breathing slows, and you focus on the sounds around you, you think you hear the sound of heavy foot steps prowling closer.\n" +
+                                  "They are still a little ways away, for now.\n" +
+                                  "What do you do now?\n " +
+                                  "1. Panic, run, hope it doesn't catch you.\n" +
+                                  "2. Remain calm, look for a hiding place, anywhere to disappear.\n" +
+                                  "3. Stand and fight.");
                 choices1_3();
-                }
-                else if (userInput == 4) {
-                    Console.WriteLine("You have rest for an hour. You hear a noise coming from behind you, you are being hunted.");
-                    player.PlayerRest(player.health);
-                }
-                else
-                {
-                    Console.WriteLine("That is not a valid choice, please select a valid choice.");
-                }
-            
+            }
+            else if (userInput == 4)
+            {
+                Console.WriteLine("You have rest for an hour. You hear a noise coming from behind you, you are being hunted.");
+                player.PlayerRest(player.health);
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid choice, please select a valid choice.");
+            }
+
         }
 
         public void choices1_1()
@@ -86,13 +87,17 @@ namespace TextGame
             {
                 Console.WriteLine("The water is so cold, before you make it under the water your teeth start to chatter, and you start shivering, this is not a survivable option.\n" +
                                   "What do you do?\n" +
-                                  "");
+                                  "1. Get out of the water. Get ready to fight.\n" +
+                                  "2. Get out of the water and run.\n" +
+                                  "3. Tough it out, surely it won't take long for the beast to lose interest.");
+                choices1_1_1();
             }
             else if (userInput == 2)
             {
                 Console.WriteLine("There is a beast here, and it is hunting you, this moment of fear and hesitation costs you dearly, the beast is nearly upon you.\n" +
                                   "What do you do?\n" +
                                   "");
+                choices1_1_2();
             }
             else if (userInput == 3)
             {
@@ -101,7 +106,7 @@ namespace TextGame
                                   "You are completely hidden from sight, you hear the beast get closer and closer, then, for a moment all is quiet, then the beast is upon you, you weren't buried deep enough and it found you\n" +
                                   "you are concious as it begins to consume you.");
                 Program.loseCondition();
-    
+
             }
             else
             {
@@ -150,5 +155,108 @@ namespace TextGame
                 Console.WriteLine("That is not a valid choice, please select a valid choice.");
             }
         }
+        public void choices1_1_1()
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("1");
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("2");
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid choice, please select a valid choice.");
+            }
+        }
+        public void choices1_1_2()
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("choice 1-1-2-1");
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("choice 1-1-2-2");
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine("choice 1-1-2-3");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid choice, please select a valid choice.");
+            }
+        }
+        
+        public void choices1_2_1()
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("1");
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("2");
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid choice, please select a valid choice.");
+            }
+        }
+        public void choices1_2_2()
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("1");
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("2");
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid choice, please select a valid choice.");
+            }
+        }
+        public void choices1_2_3()
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("1");
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("2");
+            }
+            else if (userInput == 3)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid choice, please select a valid choice.");
+            }
+        }
     }
 }
+
+
