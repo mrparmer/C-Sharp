63a2a1e9ea514bd1c49649b6b82e32f35d9cea45
mrparmer <37225891+mrparmer@users.noreply.github.com>
2019-05-30 16:59:42 -0400

    Basic Math Functions
    Needed a break from the text game to complete som basic challenges.

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    using System;
+
+    class FibSum
+    {
+        static int evenSum(int limit)
+        {
+            if (limit < 2)
+                return 0;
+            
+            long num1 = 0, num2 = 2;
+            long sum = num1 + num2;
+             
+            while (num2 <= limit)
+            {              
+                long num3 = 4 * num2 + num1;
+ 
+                if (num3 > limit)
+                    break;
+ 
+                num1 = num2;
+                num2 = num3;
+                sum += num2;
+            }
+
+            return (int)sum;
+        }        
+        public static void Main()
+        {
+            int limit = 4000000;
+            Console.Write(evenSum(limit));
+
+        }
+    }
+}
---
+++ MultiplesOf3and5/MultiplesOf3and5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplesOf3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiple = 0;
+            for (int i = 0; i < 1000; i++ )
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    multiple += i;
+                }
+            }
+            Console.WriteLine(multiple);
+        }
+    }
+}
