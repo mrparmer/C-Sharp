fbf0217c91dde28cc8a602f33e6d8dccc671c640
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2019-06-05 21:30:31 -0400

    String Reversal
    Practices reversing strings with arrays

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a word that you want reversed.");
+            string word = Console.ReadLine();
+            stringReversal(word);
+            Console.ReadKey();
+        }
+        public static string stringReversal(string str)
+        {
+            
+            char[] chars = str.ToCharArray();
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                chars[i] = str[j];
+                chars[j] = str[i];
+            }
+            Console.WriteLine(chars);
+            return new string (chars);
+            
+        }
+    }
+}
