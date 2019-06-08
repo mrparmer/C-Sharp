d8a208e824cbdd356c66e693d95be7ef1aa75485
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2019-06-08 15:11:38 -0400

    Simple Array Sorting
    Work with simple sorting as a quick review of the concept

---
+++ ArraySorting/ArraySorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArraySorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AscAlpha();
+            DesAlpha();
+        }
+
+        static void AscAlpha()
+        {
+            char[] alphabet = new char[6] { 'a', 'd', 'b', 'c', 'f', 'e' };
+            Array.Sort(alphabet);
+            foreach (char i in alphabet) Console.WriteLine(i + " ");
+            Console.ReadLine();
+        }
+
+        static void DesAlpha()
+        {
+            char[] alphabet = new char[] { 'a', 'd', 'b', 'c', 'f', 'e' };
+            Array.Sort(alphabet);
+            Array.Reverse(alphabet);
+            foreach (char i in alphabet) Console.WriteLine(i + " ");
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
