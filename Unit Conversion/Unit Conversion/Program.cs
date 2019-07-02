8037c0263a6258996494fb76c8b6cf8b2e4337d5
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2019-07-02 13:52:22 -0400

    Finished Unit Conversion App
    Finished the basic console app version

--- Unit Conversion/Unit Conversion/Program.cs
+++ Unit Conversion/Unit Conversion/Program.cs
@@ -12,6 +12,7 @@ namespace Unit_Conversion
         static void ConversionSelection()
         {
             ToEmperial ToEmp = new ToEmperial();
+            ToMetric ToMet = new ToMetric();
 
             Console.WriteLine("What units are you wanting to convert?\n" +
                 "1. Emperial to Metric\n" +
@@ -23,7 +24,7 @@ namespace Unit_Conversion
             }
             else if (selection == 2)
             {
-                Console.WriteLine("Selected 2");
+                ToMet.conversion();
             }
             else
             {
@@ -36,14 +37,14 @@ namespace Unit_Conversion
     }
     public class ToEmperial
     {
-       public void conversion()
+        public void conversion()
         {
             Console.WriteLine("What unit do you want to convert to?\n" +
                 "1. Millimeters to Inches\n" +
                 "2. Meters to Feet\n" +
-                "3. Miles\n" +
-                "4. Ounces\n" +
-                "5. Pounds");
+                "3. Kilometers to Miles\n" +
+                "4. Grams to Ounces\n" +
+                "5. Kilograms to Pounds");
             int selection = Convert.ToInt32(Console.ReadLine());
             if (selection == 1)
             {
@@ -87,19 +88,76 @@ namespace Unit_Conversion
                 string unit = "pounds";
                 Console.WriteLine(converted + " " + unit);
             }
-            else                
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Please enter a valid selection.");
-            Console.BackgroundColor = ConsoleColor.Black;
-            conversion();
+            else
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Please enter a valid selection.");
+                Console.BackgroundColor = ConsoleColor.Black;
+                conversion();
+
+            }
         }
     }
     class ToMetric
     {
         static void conversion()
         {
-            
+            Console.WriteLine("What unit do you want to convert to?\n" +
+                "1. INches to Millimeters\n" +
+                "2. Feet to Meters\n" +
+                "3. Miles to Kilometers\n" +
+                "4. Ounces to Grams\n" +
+                "5. Pounds to Kilograms");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            if (selection == 1)
+            {
+                Console.WriteLine("How many Inches to convert to Millimeters?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                double converted = number * .393701;
+                string unit = " millimeters";
+                Console.WriteLine(converted + unit);
+            }
+            else if (selection == 2)
+            {
+                Console.WriteLine("How many Feet to convert to Meters?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                double converted = number * .3048;
+                string unit = " meters";
+                Console.WriteLine(converted + unit);
+            }
+            else if (selection == 3)
+            {
+                Console.WriteLine("How many Miles to convert to Kilometers?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                double converted = number * 1.60934;
+                string unit = " meters";
+                Console.WriteLine(converted + unit);
+            }
+            else if (selection == 4)
+            {
+                Console.WriteLine("How many Ounces to convert to Grams?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                double converted = number * 28.3495;
+                string unit = " grams";
+                Console.WriteLine(converted + unit);
+            }
+            else if (selection == 5)
+            {
+                Console.WriteLine("How many Pounds to convert to Kilograms?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                double converted = number * .453592;
+                string unit = " kilograms";
+                Console.WriteLine(converted + unit);
+            }
+            else
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Please enter a valid selection.");
+                Console.BackgroundColor = ConsoleColor.Black;
+                conversion();
+            }
         }
     }
 
